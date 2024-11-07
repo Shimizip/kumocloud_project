87024a55034617596e53eee41b6b74e7f382b421
Shimi <szymon.jozef.banasiak@outlook.com>
2024-11-07 20:35:15 +0100

    CSV Upload und Check funkntionieren

--- app/services/csv_upload_service.py
+++ app/services/csv_upload_service.py
@@ -9,8 +9,19 @@ uploaded_csv_data = {}
 class CSVUploadService:
     @staticmethod
     def process_csv(file: UploadFile):
+        """
+        Liest die hochgeladene CSV-Datei ein und speichert die Daten für die spätere Verwendung.
+
+        Args:
+            file (UploadFile): Die hochgeladene CSV-Datei.
+
+        Returns:
+            None: Die Daten werden in der globalen Variable gespeichert.
+        """
         content = BytesIO(file.file.read())
-        df = pd.read_csv(content)
+        
+        # Hier kannst du das Trennzeichen anpassen, falls nötig
+        df = pd.read_csv(content, sep=';')  # Angenommen, die CSV ist durch Semikolon getrennt
         
         # Speichere die Daten in der globalen Variable
         uploaded_csv_data["data"] = df
